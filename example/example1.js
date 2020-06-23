ba8767f3ab565df34b244fae7c5f32ddfa6e5dc0
Pedro Dias <petermdias@gmail.com>
2020-06-23 19:51:51 +0100

    output treatment

--- example/example1.js
+++ example/example1.js
@@ -2,13 +2,14 @@ var Supportpal = require('../lib/supportpal');
 
 var support = new Supportpal({ 'hostname': process.env.HOST, 'port': 443, 'hash': process.env.HASH });
 
-/*
+
 support.settings.get(function (err, data) {
   console.log(err)
   console.log(data)
 });
-*/
 
+
+/*
 var opts = {
   'subject': 'subject testtttt',
   'text': 'text testtttttt',
@@ -23,3 +24,4 @@ support.ticket.create(opts, function(err, data) {
   console.log(err)
   console.log(data)
 });
+*/
